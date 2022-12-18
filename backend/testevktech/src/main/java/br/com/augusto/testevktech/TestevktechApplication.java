3fe97020d3be5e4aa8b04a9e0c7940a8abde238e
AkaAugustoNeves <augusto.nevesti@gmail.com>
2022-12-18 13:33:05 -0300

    [BACKEND] - criação do projeto backend

---
+++ backend/testevktech/src/main/java/br/com/augusto/testevktech/TestevktechApplication.java
@@ -0,0 +1,13 @@
+package br.com.augusto.testevktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestevktechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestevktechApplication.class, args);
+	}
+
+}
---
+++ backend/testevktech/src/test/java/br/com/augusto/testevktech/TestevktechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.augusto.testevktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestevktechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
